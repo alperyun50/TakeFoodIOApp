25ae0e41ed58aea75c04d28189534e27d8175354
alper yavuz ün <yavuzun50@hotmail.com>
2022-03-10 20:22:14 +0300

    models ef packages and repositories added

---
+++ TakeFoodIOApp/Data/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakeFoodIOApp.Data.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=ALPER\\ALPERNET; database=TakeFoodDB; integrated security=true;");
+        }
+
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ TakeFoodIOApp/Repositories/FoodRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TakeFoodIOApp.Data.Models;
+
+namespace TakeFoodIOApp.Repositories
+{
+    public class FoodRepository
+    {
+        Context c = new Context();
+
+        public void FoodAdd(Food f)
+        {
+            c.Foods.Add(f);
+            c.SaveChanges();
+        }
+
+        public void FoodDelete(Food f)
+        {
+            c.Foods.Remove(f);
+            c.SaveChanges();
+        }
+
+        public void FoodUpdate(Food f)
+        {
+            c.Foods.Update(f);
+            c.SaveChanges();
+        }
+
+        public List<Food> FoodList()
+        {
+            return c.Foods.ToList();
+        }
+
+        public void GetFood(int id)
+        {
+            c.Foods.Find(id);
+        }
+    }
+}
